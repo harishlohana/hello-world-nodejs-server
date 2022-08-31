e085fe3d1f627df74ca59e74e1ceeae69d17ff1c
unknown <harish.lohana@gmail.com>
2022-08-31 23:42:52 +0500

    Minor changes

--- server.mjs
+++ server.mjs
@@ -2,9 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/home', (req, res) => {
+  res.send('Hello World My First Node JS!')
 })
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
